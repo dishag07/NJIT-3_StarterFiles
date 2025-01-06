f80a5bc2ff25301004c495ddfd3a03256ec0fc76
dishag07 <dishag422@gmail.com>
2025-01-06 10:10:46 -0500

    broken?

--- site.js
+++ site.js
@@ -20,7 +20,6 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
-
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
@@ -30,18 +29,71 @@ const vue_app = Vue.createApp({
             })
       },
       data() {
-      
             return {
                   // This holds your movies.json data.
                   movies: [],
                   /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-                  title: "IMB + Disha's Top 8 Movies",
-                  owner: "Disha",
-                  repository: "https://github.com/dishag07"
+                  title: "IMB + Liz's Top 8 Movies",
+                  owner: "Liz",
+                  repository: "https://github.com/lizreznik/NJIT-Project-2"
             }
       },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dateArray) {
+                  const year = dateArray[0]
+                  let month = ""
+                  switch (dateArray[1]) {
+                        case 1:
+                              month = "January"
+                              break
+                        case 2:
+                              month = "February"
+                              break
+                        case 3:
+                              month = "March"
+                              break
+                        case 4:
+                              month = "April"
+                              break
+                        case 5:
+                              month = "May"
+                              break
+                        case 6:
+                              month = "June"
+                              break
+                        case 7:
+                              month = "July"
+                              break
+                        case 8:
+                              month = "August"
+                              break
+                        case 9:
+                              month = "September"
+                              break
+                        case 10:
+                              month = "October"
+                              break
+                        case 11:
+                              month = "November"
+                              break
+                        case 12:
+                              month = "December"
+                              break
+                  }
+                  return month + " " + dateArray[2] + ", " + dateArray[0]
+            },
+            posterClick(index) {
+                  if (this.movies[index].posterindex > this.movies[index].posters.length - 2) {
+                        this.movies[index].posterindex = 0
+                  } else {
+                        this.movies[index].posterindex++
+                  }
+            },
+            textTime(minutes) {
+                  let hours = Math.floor(minutes / 60)
+                  let min = minutes % 60
+                  return hours + ' hours, ' + min + ' minutes'
+            }
       }
 })
-vue_app.mount("#vue_app")
